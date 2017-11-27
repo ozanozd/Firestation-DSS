0f74ed022e01a1634678a3650cd501cbb1415788
Yalcin Kilic <yalcinkilic@sabanciuniv.edu>
2017-11-27 15:31:33 +0300

    write_excel
    General description is created , real functionality later:)

---
+++ write_excel.py
@@ -0,0 +1,19 @@
+"""
+The program writes the appropriate pairs into an excel sheet
+"""
+
+#Note that we just need to use this program ones.
+#Works to be done:
+    #i)  Wrote the function:)
+    #ii) Test it
+
+
+def excel_write(filename):
+    """
+    This function writes appropriate pairs into an excel sheet.
+    It takes one argument:
+        i)filename(string) : name of the excel sheet which will be created after the function finishes its job.
+    """
+
+    #Until we wrote the body of the function,do not give an error.
+    pass
