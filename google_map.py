5d0c9e85d96e50c4e761496b6c38805b1ce7af2a
Yalcin Kilic <yalcinkilic@sabanciuniv.edu>
2017-12-13 15:28:09 +0300

    istanbul map
    Dynamic map for istanbul added, zoom does not work very well.Pinning functionality should be added .

---
+++ google_map.py
@@ -0,0 +1,16 @@
+"""
+This progmram shows a istanbul using google static map API
+"""
+
+import gmplot
+
+latitudes = [ 65.32 , 64.31]
+longitudes = [ 26.15 , 27.63]
+gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
+
+gmap.plot(latitudes, longitudes, 'cornflowerblue', edge_width=10)
+gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
+gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
+gmap.heatmap(heat_lats, heat_lngs)
+
+gmap.draw("mymap.html")
