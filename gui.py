3c614bfd27abb77c65a9812153e220d6642c2239
Yalcin Kilic <yalcinkilic@sabanciuniv.edu>
2018-02-12 22:53:22 +0300

    gui map
    No functional change

--- gui.py
+++ gui.py
@@ -6,10 +6,95 @@ This program creates necessary gui for the decision support system.It based on t
 # ii) Edit radiobutton in a way that only one of them can be selected
 
 
-from tkinter import *
-import tkinter
+import tkinter as tk
 from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
+import write_excel_availability_matrix as writer
 import os.path
+
+#Define height and width as constants
+HEIGHT = 400
+WIDTH = 415
+
+class MainApplication:
+    def __init__(self , master):
+        self.master = master
+        self.canvas = tk.Canvas(master , height = HEIGHT , width = WIDTH)
+
+        # Define labels for Radiobuttons and Checkbuttons , threshold
+        self.label_left = tk.Label(self.canvas , text = "Model Extension")
+        self.label_right = tk.Label(self.canvas , text = "Model Type")
+        self.label_threshold = tk.Label(self.canvas , text = "Threshold(m)")
+
+        # Define user entries
+        self.user_entry_box = tk.Entry(self.canvas , font = ("Calibri" , 12))
+        self.user_threshold_entry = tk.Entry(self.canvas , text = "Threshold" ,  font = ("Calibri" , 12))
+
+        # Define buttons
+        self.load_button = tk.Button(self.canvas , text = "Load"  , command = lambda: load_file(user_entry_box))
+        self.select_button = tk.Button(self.canvas , text = "Select")
+        self.runmap_button = tk.Button(self.canvas , text = "Run Map" , command =  self.run_map)
+
+        # Define checkbuttons
+        self.capacity_checkbutton = tk.Checkbutton(self.canvas , text = "Capacity")
+        self.traffic_checkbutton = tk.Checkbutton(self.canvas , text = "Traffic")
+        self.risk_checkbutton = tk.Checkbutton(self.canvas , text = "Risk Factors")
+
+        # Initialize radiobuttons as all of them are unselected
+        self.radio_button_var = tk.IntVar()
+        self.radio_button_var.set(2356)
+
+        # Define radio_buttons
+        self.single_coverage_radiobutton = tk.Radiobutton(self.canvas , text = "Single Coverage" , value = 1 , variable = self.radio_button_var)
+        self.multi_coverage_radiobutton = tk.Radiobutton(self.canvas , text = "Multi Coverage" , value = 2 , variable = self.radio_button_var)
+
+
+        # Place them
+        self.user_entry_box.place(x = 25 , y = 10 , width = 260 , height = 30)
+        self.user_threshold_entry.place(x = 340 , y = 275 , width = 50 , height = 25)
+        self.label_threshold.place(x = 250 , y = 275)
+        self.load_button.place(x = 350 , y = 10 , height = 30)
+        self.select_button.place(x = 300 , y = 10 , height = 30)
+
+        self.label_left.place(x = 50 , y = 60)
+        self.capacity_checkbutton.place(x = 50 , y = 90)
+        self.traffic_checkbutton.place(x = 50 , y = 110)
+        self.risk_checkbutton.place(x = 50 , y = 130)
+
+        self.label_right.place(x = 250 , y = 60)
+        self.single_coverage_radiobutton.place(x = 250 , y = 90)
+        self.multi_coverage_radiobutton.place(x = 250 , y = 110)
+
+        self.runmap_button.place(x = 180 ,  y = 350)
+        self.canvas.pack()
+
+    def check_validity_threshold(self):
+        """
+        This function checks validity of the user's threshold
+        """
+        try :
+            int(self.user_threshold_entry.get())
+            return True
+        except ValueError :
+            return False
+
+        return True
+    def run_map(self):
+        if self.check_validity_threshold() == False :
+            messagebox.showerror("Wrong type!" , "Please enter an integer value.")
+            self.user_threshold_entry.delete(0 , len(self.user_threshold_entry.get()))
+
+def main():
+    root = tk.Tk()
+    app = MainApplication(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
 def load_file(user_entry_box):
     """
@@ -40,11 +125,14 @@ def select_threshold(user_threshold_entry):
     if check_validity_threshold(user_threshold_entry) == False:
         pass
 
-    file_path =  os.path()
+    filename = "availability_matrix_" + user_threshold_entry.get() + "xlsx"
+    file_path =  os.path(filename)
 
-    if os.path.exists(file_path)
-
-
+    # If the file is already exists use it.Otherwise , create it
+    if os.path.exists(file_path) == True :
+        pass
+    else :
+        writer.run()
 def select_file():
     """
     When the select button is pressed this function is invoked.It is responsible for finalizing the file to load the file.
@@ -80,49 +168,3 @@ def run_map():
     When the run map button is selected this function is invoked.
     """
     pass
-
-def prepare_gui():
-    """
-    This function creating gui items and initialize them
-    """
-    top = tkinter.Tk()
-    # Code to add widgets will go here...
-    canvas = Canvas(top , height = 450 , width = 450)
-    label_left = Label(top , text = "Model Extension" ,  underline = 0)
-    label_right = Label(top , text = "Model Type" ,   underline = 0)
-    label_threshold = Label(top , text = "Threshold(m)")
-    user_entry_box = Entry(top , font = ("Calibri" , 12))
-    user_threshold_entry = Entry(top , text = "Threshold" ,  font = ("Calibri" , 12) , command = lambda: check_validity_threshold(user_threshold_entry) )
-    load_button = Button(top , text = "Load"  , command = lambda: load_file(user_entry_box))
-    select_button = Button(top , text = "Select" )
-    threshold_select_button = Button(top , text = "Select" , command = lambda: select_threshold(user_threshold_entry))
-    runmap_button = Button(top , text = "Run Map"  )
-    capacity_checkbutton = Checkbutton(top , text = "Capacity")
-    traffic_checkbutton = Checkbutton(top , text = "Traffic")
-    risk_checkbutton = Checkbutton(top , text = "Risk Factors")
-    single_coverage_radiobutton = Radiobutton(top , text = "Single Coverage")
-    multi_coverage_radiobutton = Radiobutton(top , text = "Multi Coverage")
-
-
-    user_entry_box.place(x = 25 , y = 10 , width = 260 , height = 30)
-    user_threshold_entry.place(x = 300 , y = 250 , width = 50 , height = 25)
-    label_threshold.place(x = 220 , y = 255)
-    threshold_select_button.place(x = 360 , y = 250 )
-    load_button.place(x = 350 , y = 10 , height = 30)
-    select_button.place(x = 300 , y = 10 , height = 30)
-
-    label_left.place(x = 50 , y = 60)
-    capacity_checkbutton.place(x = 50 , y = 90)
-    traffic_checkbutton.place(x = 50 , y = 110)
-    risk_checkbutton.place(x = 50 , y = 130)
-
-    label_right.place(x = 250 , y = 60)
-    single_coverage_radiobutton.place(x = 250 , y = 90)
-    multi_coverage_radiobutton.place(x = 250 , y = 110)
-
-    runmap_button.place(x = 150 ,  y = 300)
-
-    canvas.pack()
-    top.mainloop()
-
-prepare_gui()
--- map.py
+++ map.py
@@ -1,17 +1,16 @@
+
 """
 This program open a map using foluim library
 """
+
 import folium
 import webbrowser
 import os
 import pandas as pd
 
-state_geo = os.path.join('data' , 'cities_of_turkey.json')
+
 m = folium.Map(location = [41.015137, 28.979530])
-folium.GeoJson(
-    state_geo ,
-    name = 'geojson'
-).add_to(m)
+folium.Marker(location = [41.015137, 28.979530]).add_to(m)
 m.save("istanbul_map.html")
 path = os.path.abspath("istanbul_map.html")
 url = "file://" + path
