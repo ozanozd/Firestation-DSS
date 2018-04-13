3ea7dbe3f241bb44ceb5d1b75be71ecf10fdfd5b
Yalcin Kilic <yalcinkilic@sabanciuniv.edu>
2018-04-13 16:40:49 +0300

    Queries
    Queries

--- google_query.py
+++ google_query.py
@@ -23,7 +23,13 @@ API_KEYS = ['AIzaSyBGWTZbOAUijqF5J6SwhF-nYAEzxCljLgU' , 'AIzaSyDtJI-Yg2mnCKJjY_6
             'AIzaSyDGSVxQTgvxTEvWfU2En9f9OcS0pji0Kds' , 'AIzaSyDp7SDpmSNjjC29nKBMC9sDzzXctSHcFDM' , 'AIzaSyDVkHOgIaBpadExBHPWmm3gvnQXsg12bJc',
             'AIzaSyCaC_cGqWwycn_EGuzmi3c33V4HOZW3QOk' , 'AIzaSyB1-xyQbKs6MWhUq-arTdIKN5T6w4nbKw0' , 'AIzaSyBl2euWfLfFC8SlCaWToAKlC-VLK5inaIg',
             'AIzaSyAAqut4XTcwfILCxURFFSzzvGifv_B_TDg' , 'AIzaSyBOfXybp-Snv0x8wbiiuQLO4hCFv9fygBo' , 'AIzaSyAB_0DsDpvs5gtYiWXQ5qR79_snEfckAd4',
-            'AIzaSyApRJCgE9T4uwezUWmFA1o_TxdlAR-pomM' , 'AIzaSyDFVNH1GvqenvjhEcNODjC57J9GX5ggp5s' , 'AIzaSyDBDcGZsHYUQ31BBfv0TWoowejwTOY4xdo']
+            'AIzaSyApRJCgE9T4uwezUWmFA1o_TxdlAR-pomM' , 'AIzaSyDFVNH1GvqenvjhEcNODjC57J9GX5ggp5s' , 'AIzaSyDBDcGZsHYUQ31BBfv0TWoowejwTOY4xdo',
+            'AIzaSyBNukU73E7cywjsm2WvQE_sU8nDTCBSXbc' , 'AIzaSyDXWJdW0iiBtW5yk_j80aHzjfICjbgI8MY' , 'AIzaSyCQko6GqIns9Jl-64iW8HSq7vP7eySA3PI',
+            'AIzaSyBAhElFu8ZK2aYQDfLuhY0vH9KSdf2TBEw' , 'AIzaSyABT6KXM1hCyDTTxKg9Ewg637vzkImtmmA' , 'AIzaSyAU7QXh95Co-f6_1nCWPFJEPAP8L0jkrfg',
+            'AIzaSyA6jmERnzCpAPjrXXUDj2DpOTr0radPHuQ' , 'AIzaSyAChYb-7Tqkeo6vlFr8L-9uJhNq-I6QhCk' , 'AIzaSyD96kPFRgmce2t9QVrVBcm4VvzOwgM5myM',
+            'AIzaSyC0K18xKPd2nj0Vf62EWBpdFzmAoMgh_pc' , 'AIzaSyAI2y3a1df7GA3lQ5Ck4pH1nN-vWNDoIzk' , 'AIzaSyD8QygMGLCAI_dLFzS02FJ6th3Dvx1wGGU',
+            'AIzaSyC6wUgQ_YRk21CP_gYZ6Db63M06IX9RT2c' , 'AIzaSyCFyMW-r7flVucZHHm08Z1y9GqnRHHfH94' , 'AIzaSyBZVipLbax-Ezhm_L_V3Iziz9TEyktmbjI',
+            'AIzaSyBgzFdlktfA2iIzwiga9EN95-j0exA-szo']
 
 def query_all_appropriate_pairs(appropriate_pairs , x_coordinates  , y_coordinates):
     """
@@ -31,16 +37,22 @@ def query_all_appropriate_pairs(appropriate_pairs , x_coordinates  , y_coordinat
     """
 
     # j is the API_KEYS index
-    j = 0
+    j = 11
     i = 0
     results = []
-    try:
-        gmaps = googlemaps.Client(key = API_KEYS[j])
-    except :
-        print("API key exception caugth.")
-        j += 1
-        gmaps = googlemaps.Client(key = API_KEYS[j] )
+    is_valid = False
+    while is_valid == False :
+
+        try:
+            gmaps = googlemaps.Client(key = API_KEYS[j])
+            is_valid = True
+        except :
+            print("API key exception caugth.")
+            j += 1
+            j = j % len(API_KEYS)
     while i < len(appropriate_pairs):
+        is_valid = False
+        enter_if = False
         district1_index = appropriate_pairs[i][0]
         district2_index = appropriate_pairs[i][1]
         x_coord1 = y_coordinates[district1_index - 1]
@@ -51,10 +63,14 @@ def query_all_appropriate_pairs(appropriate_pairs , x_coordinates  , y_coordinat
         destinations = []
         origins.append(str(x_coord1) + ' ' + str(y_coord1))
         destinations.append(str(x_coord2) + ' ' + str(y_coord2))
-        directions_result = gmaps.distance_matrix(origins , destinations , mode='driving')
+        try:
+            directions_result = gmaps.distance_matrix(origins , destinations , mode='driving')
+            enter_if = True
+        except:
+            results.append("0 mins")
         if i % 100 == 0 :
             print("Completed" , i , "queries")
-        if directions_result['rows'][0]['elements'][0]['status'] == "OK" and directions_result['status'] == "OK" :
+        if  enter_if == True and directions_result['rows'][0]['elements'][0]['status'] == "OK" and directions_result['status'] == "OK" :
             results.append(directions_result['rows'][0]['elements'][0]['duration']['text'])
             i += 1
         else :
@@ -65,12 +81,16 @@ def query_all_appropriate_pairs(appropriate_pairs , x_coordinates  , y_coordinat
         #   gmaps = googlemaps.Client(key = API_KEYS[j])
         if i % 2400 == 0 :
             j += 1
-            try:
-                gmaps = googlemaps.Client(key = API_KEYS[j])
-            except:
-                j += 1
-                gmaps = googlemaps.Client(key = API_KEYS[j])
-            print("API key chagend from" , j , "to" , j + 1 , ".")
+            j = j % len(API_KEYS)
+            while is_valid == False :
+                try:
+                    gmaps = googlemaps.Client(key = API_KEYS[j])
+                    is_valid = True
+                except:
+                    j += 1
+                    j = j % len(API_KEYS)
+
+            print("API key chagend from" , j - 1 , "to" , j  , ".")
     return results
 
 def google_query_find(from_district,to_district):
@@ -137,7 +157,7 @@ def create_data(threshold):
 
 def run():
     from_district , to_district , distances = helper.read_excel_file("MahalleVerileri.xlsx")
-    appropriate_pairs = helper.get_appropriate_pairs(from_district , to_district , distances , 5000)
+    appropriate_pairs = helper.get_appropriate_pairs(from_district , to_district , distances , 7000)
     x_coordinates , y_coordinates = helper.get_x_y_coordinates("MahalleVerileri.xlsx")
     results_array = query_all_appropriate_pairs(appropriate_pairs , x_coordinates , y_coordinates)
     writer.write_query(appropriate_pairs , results_array)
